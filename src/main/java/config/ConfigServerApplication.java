cb593dbff7c77d48a2a13af3d2e928794b93b02f
Torsten Naumann <torsten.naumann@esentri.com>
2017-12-08 11:07:15 +0100

    Initial

---
+++ src/main/java/config/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by torstennaumann on 03.11.17.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
